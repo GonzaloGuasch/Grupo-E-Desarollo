f6e41508de882342bd4cc1a15684f9103fb0835f
GonzaloGuasch <gonzaloguasch@outlook.com>
2020-09-02 16:09:19 -0400

    Spring structure

---
+++ src/main/java/ar/edu/unq/desapp/grupoE/backEnddesappapi/BackEndDesappApiApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoE.backEnddesappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndDesappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndDesappApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unq/desapp/grupoE/backEnddesappapi/BackEndDesappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoE.backEnddesappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndDesappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
