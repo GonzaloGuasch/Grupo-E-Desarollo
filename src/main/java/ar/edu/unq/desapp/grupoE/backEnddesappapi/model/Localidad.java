6ed9cb172e3e4ddffb8374fa1e04232dd10ed288
GonzaloGuasch <gonzaloguasch@outlook.com>
2020-09-09 12:59:41 -0300

    Creo objecto localidad

---
+++ src/main/java/ar/edu/unq/desapp/grupoE/backEnddesappapi/model/Localidad.java
@@ -0,0 +1,15 @@
+package ar.edu.unq.desapp.grupoE.backEnddesappapi.model;
+
+public class Localidad {
+    private String nombre;
+    private String provincia;
+    private Integer cantidadDePoblacion;
+    private Boolean estaConectada;
+
+    public Localidad(String nombre, String provincia, Integer cantidadDePoblacion, Boolean estaConectada) {
+        this.nombre = nombre;
+        this.provincia = provincia;
+        this.cantidadDePoblacion = cantidadDePoblacion;
+        this.estaConectada = estaConectada;
+    }
+}
