fadb582c3b8a8b07471b31dd2a55fe0937974531
Iara <aleiarak@gmail.com>
2020-11-18 02:22:56 -0300

    Update validaciones para Locality

--- src/main/java/ar/edu/unq/desapp/grupoE/backEnddesappapi/webservice/LocalityController.java
+++ src/main/java/ar/edu/unq/desapp/grupoE/backEnddesappapi/webservice/LocalityController.java
@@ -4,9 +4,15 @@ import ar.edu.unq.desapp.grupoE.backEnddesappapi.model.Locality;
 import ar.edu.unq.desapp.grupoE.backEnddesappapi.model.wrappers.LocalityWrapper;
 import ar.edu.unq.desapp.grupoE.backEnddesappapi.service.LocalityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -29,10 +35,22 @@ public class LocalityController {
     public List deleteByName(@PathVariable String name) {return localityService.deleteByName(name);}
 
     @PostMapping("/create")
-    public Locality create_locality(@RequestBody LocalityWrapper new_locality) {
+    public Locality create_locality(@Valid @RequestBody LocalityWrapper new_locality) {
         return localityService.save_locality(new_locality);
     }
 
     @GetMapping("/topOldestDonation")
     public List<Locality> getTopTenOfOldestDonation(){return this.localityService.getTopTenOldestdonation();}
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
 }
