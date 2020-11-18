33b2949a025793644f4ddaa507ea9495f9bb1890
Iara <aleiarak@gmail.com>
2020-11-18 02:21:17 -0300

    Agrego validaciones para Donation

--- src/main/java/ar/edu/unq/desapp/grupoE/backEnddesappapi/model/DonationRecordEntry.java
+++ src/main/java/ar/edu/unq/desapp/grupoE/backEnddesappapi/model/DonationRecordEntry.java
@@ -4,18 +4,25 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import java.time.Month;
 import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class DonationRecordEntry {
 
     @Column
+    @NotBlank(message = "Project Name is required")
     private String projectName;
 
     @Column
     @JsonFormat(pattern = "dd-MM-yyyy")
+    @NotNull(message = "Donation Date is required")
     private LocalDate donationDate;
 
     @Column
+    @NotNull(message = "Donation Amount is required")
+    @Min(value = 2, message = "Minimum amount is 2")
     private Integer donatedAmount;
 
     @Id
--- src/main/java/ar/edu/unq/desapp/grupoE/backEnddesappapi/webservice/DonationEntryController.java
+++ src/main/java/ar/edu/unq/desapp/grupoE/backEnddesappapi/webservice/DonationEntryController.java
@@ -3,9 +3,15 @@ package ar.edu.unq.desapp.grupoE.backEnddesappapi.webservice;
 import ar.edu.unq.desapp.grupoE.backEnddesappapi.model.DonationRecordEntry;
 import ar.edu.unq.desapp.grupoE.backEnddesappapi.service.UserDonationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/donationEntry")
@@ -20,12 +26,23 @@ public class DonationEntryController {
     }
 
     @PostMapping("/create")
-    public DonationRecordEntry create(@RequestBody DonationRecordEntry donationRecordEntryToSave){
+    public DonationRecordEntry create(@Valid @RequestBody DonationRecordEntry donationRecordEntryToSave){
         return this.userDonationService.save(donationRecordEntryToSave);
     }
 
     @GetMapping("/topTenDonation")
     public List<DonationRecordEntry> getTopTen(){return this.userDonationService.getTopTenDonations();}
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
 
 }
