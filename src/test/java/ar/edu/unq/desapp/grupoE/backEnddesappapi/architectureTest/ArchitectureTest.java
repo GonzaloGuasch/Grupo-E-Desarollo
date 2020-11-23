848b9ec46e0f75e944804dc00f3c3c869ad69166
iariiale <aleiarak@gmail.com>
2020-11-23 20:29:17 -0300

    Agrego test de arquitectura

--- src/test/java/ar/edu/unq/desapp/grupoE/backEnddesappapi/architectureTest/ArchitectureTest.java
+++ src/test/java/ar/edu/unq/desapp/grupoE/backEnddesappapi/architectureTest/ArchitectureTest.java
@@ -3,8 +3,9 @@ package ar.edu.unq.desapp.grupoE.backEnddesappapi.architectureTest;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
 
 @AnalyzeClasses(packages = "ar.edu.unq.desapp.grupoE.backEnddesappapi")
 public class ArchitectureTest {
@@ -13,5 +14,16 @@ public class ArchitectureTest {
     public static final ArchRule class_and_package_contaiment_checks = classes()
             .that().haveSimpleNameStartingWith("LocalityService")
             .should().resideInAPackage("ar.edu.unq.desapp.grupoE.backEnddesappapi.service");
+
+
+    @ArchTest
+    public static final ArchRule code_units_in_service_layer_should_be_autowired =
+            noCodeUnits().that().areDeclaredInClassesThat().resideInAPackage("ar.edu.unq.desapp.grupoE.backEnddesappapi.service")
+                    .should().beMetaAnnotatedWith(Autowired.class);
+
+    @ArchTest
+    public static final ArchRule services_should_not_access_controllers =
+            noClasses().that().resideInAPackage("..ar.edu.unq.desapp.grupoE.backEnddesappapi.service")
+                    .should().accessClassesThat().resideInAPackage("..ar.edu.desapp.grupoE.backEnddesappapi.webservice");
 
 }
